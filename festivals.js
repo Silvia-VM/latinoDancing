deb9d192bf1266202d15e332c6b44eb19c76e960
silvia <silvia75011@hotmail.fr>
2019-05-26 20:25:36 +0200

    ajout lieux,liens

--- festivals.js
+++ festivals.js
@@ -10,21 +10,14 @@ const scrape = async () => {
   let mois_tab = [];
   let date_tab = [];
   let localisation_tab = [];
+  let lieu_tab = [];
   let liens_tab = [];
+  let site_tab = [];
 
-  // let desc_tab = [];
-  // let horairePrix_tab = [];
-
-  const titleFestival = $(".festival");
   const mois = $(".date");
-  const date = $(".festival");
-  const localisation = $(".festival");
-  const liens = $(".festival");
+  const festival = $(".festival");
 
-  // const nomDanses = $(".grid7 .soiree-info .description p strong");
-  // const horairePrix = $(".grid7 .soiree-info .description p strong");
-
-  titleFestival.each((i, el) => {
+  festival.each((i, el) => {
     const festivalDanse = $(el)
       .children("h2")
       .text();
@@ -38,53 +31,41 @@ const scrape = async () => {
     mois_tab.push(moisTitre);
   });
 
-  date.each((i, el) => {
+  festival.each((i, el) => {
     const jours = $(el).text();
     date_tab.push(jours.match(/Du(.*)à/)[1]);
   });
 
-  localisation.each((i, el) => {
+  festival.each((i, el) => {
     const lieulocalisation = $(el)
       .find("a")
       .attr("href");
     localisation_tab.push(lieulocalisation);
   });
 
-  liens.each((i, el) => {
-    var elements = $(el).children("a");
-    // console.log(elements);
-    elements.each(function(item) {
-      item = $(this).text();
-      // .match(/http(.*)/);
-      liens_tab.push(
-        item
-        // .find("a")
-        // .attr("href")
-      );
-    });
-    // console.log(liens_tab);
+  festival.each((i, el) => {
+    const lieu_element = $(el)
+      .children("a")
+      .text();
+    lieu_tab.push(lieu_element.split("#")[0]);
+    site_tab.push(lieu_element.split(/#../)[1]);
   });
 
-  // nomDanses.each((i, el) => {
-  //   const nomSoiree = $(el)
-  //     .children(".label")
-  //     .text();
-  //   nomDanses_tab.push(nomSoiree);
-  // });
-
-  // horairePrix.each((i, el) => {
-  //   const horaires = $(el).text();
-  //   horairePrix_tab.push(horaires.split("  ")[1]);
-  // });
+  festival.each((i, el) => {
+    var elements = $(el).children("a");
+    elements.each(function(item) {
+      item = $(this).text();
+      liens_tab.push(item);
+    });
+  });
 
   // console.log(titleFestival_tab);
   // console.log(mois_tab);
   // console.log(date_tab);
-  console.log(localisation_tab);
+  // console.log(localisation_tab);
+  // console.log(lieu_tab);
+  // console.log(site_tab);
   // console.log(liens_tab);
-
-  // console.log(desc_tab);
-  //   console.log(nomDanses_tab);
 };
 
 scrape();
